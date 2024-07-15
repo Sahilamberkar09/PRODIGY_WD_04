b42dfef7739d3a4b45d28fe3164ebdbf22a7b222
Sahil Amberkar <123722453+Sahilamberkar09@users.noreply.github.com>
2024-07-16 01:19:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const navLinks = document.querySelector(".navbar ul");
 const wrapper = document.querySelector(".site-wrapper");
 const mNavLinks = document.querySelector(".mobile-navbar");
 
+const home = document.querySelector(".mob-home");
+const about = document.querySelector(".mob-about");
+const projects = document.querySelector(".mob-projects");
+const skills = document.querySelector(".mob-skills");
+
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 
@@ -15,4 +20,36 @@ hamburger.addEventListener("click", () => {
   } else {
     hamChild.classList.replace("fa-xmark", "fa-bars");
   }
+});
+
+home.addEventListener("click", () => {
+  navLinks.classList.remove("active");
+  wrapper.classList.remove("site-wrapper-active");
+  mNavLinks.classList.remove("mobile-navbar-active");
+  let hamChild = hamburger.firstElementChild;
+  hamChild.classList.replace("fa-xmark", "fa-bars");
+});
+
+about.addEventListener("click", () => {
+  navLinks.classList.remove("active");
+  wrapper.classList.remove("site-wrapper-active");
+  mNavLinks.classList.remove("mobile-navbar-active");
+  let hamChild = hamburger.firstElementChild;
+  hamChild.classList.replace("fa-xmark", "fa-bars");
+});
+
+projects.addEventListener("click", () => {
+  navLinks.classList.remove("active");
+  wrapper.classList.remove("site-wrapper-active");
+  mNavLinks.classList.remove("mobile-navbar-active");
+  let hamChild = hamburger.firstElementChild;
+  hamChild.classList.replace("fa-xmark", "fa-bars");
+});
+
+skills.addEventListener("click", () => {
+  navLinks.classList.remove("active");
+  wrapper.classList.remove("site-wrapper-active");
+  mNavLinks.classList.remove("mobile-navbar-active");
+  let hamChild = hamburger.firstElementChild;
+  hamChild.classList.replace("fa-xmark", "fa-bars");
 });
